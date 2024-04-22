526b9b9ab2948d9d56d535a497f3a3dd3d928c20
bayanban <markestevenbayanban@gmail.com>
2024-04-22 18:44:12 +0800

    change name

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('DB_USERNAME', '');
 define('DB_SERVER', 'localhost');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'licera');
+define('DB_NAME', 'bayanban');
  
 try{
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
